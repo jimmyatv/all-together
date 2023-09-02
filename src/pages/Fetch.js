9cfd60232ebd573335494a17a9d1840e44726ff5
jimmyatv <rasic.alexandar@gmail.com>
2023-09-02 12:25:41 +0200

    Fetch and hooks: useEffect and useMemo | in progress

--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,9 +1,29 @@
-import React from 'react';
+import {React, useState, useEffect, useMemo} from 'react';
 
 const Fetch = () => {
+
+    const [universities, setUniversities] = useState([]);
+
+  fetch('http://universities.hipolabs.com/search?country=United+States')
+  .then(res => res.json())
+  .then(unix => {
+    setUniversities(unix)
+  });
+
+
   return (
     <div>
-        Fetch
+        <input type='text' placeholder='Search' /> 
+        <hr/>
+        <ul style={{listStyle:'none'}}>
+        {universities.map((uni, idx) => {
+          return(
+            <li key={idx}>
+              {uni.name}
+            </li>
+          )
+        })}
+        </ul>
     </div>
   )
 };
