6cbab03413409b60b0a89d22a3b8973cdd1dc252
jimmyatv <rasic.alexandar@gmail.com>
2023-09-02 17:43:24 +0200

    useEffect and univerisites fetch | ADDED

--- src/pages/Fetch.js
+++ src/pages/Fetch.js
@@ -1,29 +1,42 @@
-import {React, useState, useEffect, useMemo} from 'react';
+import { React, useState, useEffect, useMemo } from 'react';
 
 const Fetch = () => {
 
-    const [universities, setUniversities] = useState([]);
+  const [universities, setUniversities] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  fetch('http://universities.hipolabs.com/search?country=United+States')
-  .then(res => res.json())
-  .then(unix => {
-    setUniversities(unix)
-  });
+
+  useEffect(() => {
+
+    setLoading(true);
+    (async () => {
+      const res = await fetch('http://universities.hipolabs.com/search?country=United+States')
+      const uni = await res.json();
+      setUniversities(uni);
+      setLoading(false);
+    })();
+
+  }, []);
 
 
   return (
     <div>
-        <input type='text' placeholder='Search' /> 
-        <hr/>
-        <ul style={{listStyle:'none'}}>
-        {universities.map((uni, idx) => {
-          return(
-            <li key={idx}>
-              {uni.name}
-            </li>
-          )
-        })}
+      <input type='text' placeholder='Search' />
+      <hr />
+      {loading
+        ?
+        <h3>Loading...</h3>
+        :
+        <ul style={{ listStyle: 'none' }}>
+          {universities.map((uni, idx) => {
+            return (
+              <li key={idx}>
+                {uni.name}
+              </li>
+            )
+          })}
         </ul>
+      }
     </div>
   )
 };
